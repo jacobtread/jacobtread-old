5256f26676f9900c293b215e707daa855b6db318
Jacobtread <jacobtread@gmail.com>
2022-05-16 16:28:11 +1200

    Added KME3 Project

--- components/Projects.vue
+++ components/Projects.vue
@@ -136,6 +136,12 @@ const projects: Project[] = [
         tags: ['Kotlin', 'C++', 'Android', 'Jetpack Compose']
     },
     {
+        name: 'KME3',
+        description: 'A custom game server for Mass Effect 3 which emulates the functionality of the official EA servers but allows home hosting or lan play and editing of player data',
+        links: [githubLink('/jacobtread/KME3')],
+        tags: ['Kotlin', 'Game Server']
+    },
+    {
         name: 'TetriVersal',
         description: 'An unfinished game of multiplayer tetris intended for use as the game for a school project. Although unfinished it is completely playable it is only unfinished in terms of not having a proper client completed the only way to play is through the testing client which is a terminal client',
         links: [githubLink('jacobtread/TetriVersal')],
